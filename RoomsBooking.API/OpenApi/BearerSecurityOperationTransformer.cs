94fb50d0c9b6c82d4c78072668e893a3c0a202a4
SkyReed <itzskyreed@gmail.com>
2026-07-04 01:10:53 +0500

    feat: добавлены Bearer трансформеры для OpenApi схемы
    используется для окна с добавлением bearer токена прямо в scalar

---
+++ RoomsBooking.API/OpenApi/BearerSecurityOperationTransformer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace RoomsBooking.API.OpenApi;
+
+internal sealed class BearerSecurityOperationTransformer : IOpenApiOperationTransformer
+{
+    public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
+    {
+        var hasAuthorize = context.Description.ActionDescriptor.EndpointMetadata
+            .Any(metadata => metadata is IAuthorizeData);
+
+        var allowAnonymous = context.Description.ActionDescriptor.EndpointMetadata
+            .Any(metadata => metadata is IAllowAnonymous);
+
+        if (!hasAuthorize || allowAnonymous) return Task.CompletedTask;
+        var securityRequirement = new OpenApiSecurityRequirement
+        {
+            [new OpenApiSecuritySchemeReference("Bearer", context.Document)] = []
+        };
+
+        operation.Security ??= new List<OpenApiSecurityRequirement>();
+        operation.Security.Add(securityRequirement);
+
+        return Task.CompletedTask;
+    }
+}
---
+++ RoomsBooking.API/OpenApi/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace RoomsBooking.API.OpenApi;// Укажи свой namespace
+
+internal sealed class BearerSecuritySchemeTransformer(IAuthenticationSchemeProvider authenticationSchemeProvider) : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+        if (authenticationSchemes.All(a => a.Name != "Bearer"))
+            return;
+
+        var bearerScheme = new OpenApiSecurityScheme
+        {
+            Type = SecuritySchemeType.Http,
+            Scheme = "bearer",
+            BearerFormat = "JWT",
+            In = ParameterLocation.Header,
+            Description = "JWT Authorization header using the Bearer scheme."
+        };
+
+        document.Components ??= new OpenApiComponents();
+        document.AddComponent("Bearer", bearerScheme);
+    }
+}
