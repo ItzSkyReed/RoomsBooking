8db738a1ce93c00e2e853d6c407888a89a229af5
SkyReed <itzskyreed@gmail.com>
2026-07-04 01:09:35 +0500

    feat: добавлен argon2 для хэширования паролей

---
+++ RoomsBooking.Application/Interfaces/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace RoomsBooking.Application.Interfaces;
+
+public interface IPasswordHasher
+{
+    string Hash(string password);
+    bool Verify(string password, string hashedPassword);
+}
---
+++ RoomsBooking.Infrastructure/Authentication/Argon2PasswordHasher.cs
@@ -0,0 +1,18 @@
+﻿
+using RoomsBooking.Application.Interfaces; // Твой интерфейс из Application
+using Isopoh.Cryptography.Argon2;
+
+namespace RoomsBooking.Infrastructure.Authentication;
+
+public class Argon2PasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return Argon2.Hash(password);
+    }
+
+    public bool Verify(string password, string hashedPassword)
+    {
+        return Argon2.Verify(hashedPassword, password);
+    }
+}
