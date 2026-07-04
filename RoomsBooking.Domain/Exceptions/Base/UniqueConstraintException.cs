7f6c8cde0332e14870ee6ed5f5187aaa89ac96f7
SkyReed <itzskyreed@gmail.com>
2026-07-05 00:48:35 +0500

    feat: Добавлена обработка Unique Constraint Violation

---
+++ RoomsBooking.Domain/Exceptions/Base/UniqueConstraintException.cs
@@ -0,0 +1,7 @@
+﻿namespace RoomsBooking.Domain.Exceptions.Base;
+
+public class UniqueConstraintException(string constraintName, Exception innerException)
+    : Exception($"Нарушение уникальности данных (индекс: {constraintName})", innerException)
+{
+    public string ConstraintName { get; } = constraintName;
+}
--- RoomsBooking.Infrastructure/Persistence/AppDbContext.cs
+++ RoomsBooking.Infrastructure/Persistence/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using RoomsBooking.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 using RoomsBooking.Application.Interfaces;
+using RoomsBooking.Domain.Exceptions.Base;
 
 namespace RoomsBooking.Infrastructure.Persistence;
 
@@ -17,5 +19,19 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         base.OnModelCreating(modelBuilder);
 
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        // Ловим ошибку. В случае, если это нарушение уникальности - возвращаем собственную ошибку.
+        try
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+        catch (DbUpdateException ex)
+            when (ex.InnerException is PostgresException { SqlState: "23505" } pgEx)
+        {
+            throw new UniqueConstraintException(pgEx.ConstraintName ?? string.Empty, ex);
+        }
     }
 }
