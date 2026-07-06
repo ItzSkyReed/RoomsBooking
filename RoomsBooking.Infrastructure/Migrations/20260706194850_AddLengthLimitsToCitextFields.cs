feebbc4a9e5ecbacf2083b5d943a3ef372252ec6
SkyReed <itzskyreed@gmail.com>
2026-07-07 00:52:10 +0500

    fix: добавлены лимиты для citext полей

---
+++ RoomsBooking.Infrastructure/Migrations/20260706194850_AddLengthLimitsToCitextFields.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoomsBooking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLengthLimitsToCitextFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                @"ALTER TABLE rooms ADD CONSTRAINT CK_rooms_number_max_length CHECK (char_length(""Number"") <= 100);
+                ALTER TABLE users ADD CONSTRAINT CK_users_email_max_length CHECK (char_length(""Email"") <= 255);"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
