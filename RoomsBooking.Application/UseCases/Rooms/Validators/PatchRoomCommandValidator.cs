6297eb05c86686383efe34852f8dfad4ed7d2e64
SkyReed <itzskyreed@gmail.com>
2026-07-10 14:08:29 +0500

    fix: валидация только переданных полей в информации о комнате

--- RoomsBooking.Application/UseCases/Rooms/Validators/PatchRoomCommandValidator.cs
+++ RoomsBooking.Application/UseCases/Rooms/Validators/PatchRoomCommandValidator.cs
@@ -6,19 +6,22 @@ namespace RoomsBooking.Application.UseCases.Rooms.Validators;
 
 public class PatchRoomCommandValidator : AbstractValidator<PatchRoomCommand>
 {
-
     public PatchRoomCommandValidator()
     {
         RuleFor(x => x.Number)
             .MaximumLength(Room.MaxNumberLength).WithMessage($"Максимальная длина названия комнаты - {Room.MaxNumberLength} символов")
-            .OverridePropertyName("number");
+            .NotEmpty().WithMessage("Номер комнаты не может быть пустым.")
+            .OverridePropertyName("number")
+            .When(x => x.Number != null);
 
         RuleFor(x => x.Description)
             .MaximumLength(Room.MaxDescriptionLength).WithMessage($"Максимальная длина описания комнаты - {Room.MaxDescriptionLength} символов")
-            .OverridePropertyName("description");
+            .OverridePropertyName("description")
+            .When(x => x.IsDescriptionSet);
 
         RuleFor(x => x.Capacity)
             .GreaterThan(Room.MinCapacity).WithMessage($"Вместимость комнаты должна быть больше {Room.MinCapacity}")
-            .OverridePropertyName("capacity");
+            .OverridePropertyName("capacity")
+            .When(x => x.Capacity.HasValue);
     }
 }
