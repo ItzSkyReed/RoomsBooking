9ff14cdaf74e6e51389b977db7418de5b642fda6
SkyReed <itzskyreed@gmail.com>
2026-07-10 14:07:50 +0500

    feat: добавлена валидация для патча бронирования

---
+++ RoomsBooking.Application/UseCases/Bookings/Validators/PatchBookingCommandValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using RoomsBooking.Application.UseCases.Bookings.Commands;
+using RoomsBooking.Domain.Entities;
+
+namespace RoomsBooking.Application.UseCases.Bookings.Validators;
+
+public class PatchBookingCommandValidator : AbstractValidator<PatchBookingCommand>
+{
+    public PatchBookingCommandValidator()
+    {
+        RuleFor(x => x.StartTime)
+            .GreaterThan(DateTimeOffset.UtcNow).WithMessage("Время начала бронирования должно быть в будущем.")
+            .OverridePropertyName("startTime")
+            .When(x => x.StartTime.HasValue);
+
+        RuleFor(x => x.EndTime)
+            .GreaterThan(x => x.StartTime!.Value).WithMessage("Время окончания должно быть позже времени начала.")
+            .OverridePropertyName("endTime")
+            .When(x => x.EndTime.HasValue && x.StartTime.HasValue);
+
+        // Проверяем длительность, только если переданы ОБА значения
+        RuleFor(x => x)
+            .Must(x => x.EndTime!.Value - x.StartTime!.Value >= Booking.MinBookingInterval)
+            .WithMessage($"Интервал бронирования слишком короткий. Минимальное время: {Booking.MinBookingInterval.Minutes} минут.")
+            .Must(x => x.EndTime!.Value - x.StartTime!.Value <= Booking.MaxBookingInterval)
+            .WithMessage($"Интервал бронирования слишком долгий. Максимальное время: {Booking.MaxBookingInterval.Hours} часов.")
+            .OverridePropertyName("duration")
+            .When(x => x.StartTime.HasValue && x.EndTime.HasValue);
+    }
+}
